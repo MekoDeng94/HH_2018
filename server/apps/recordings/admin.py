786d2573e4aef86a975da42a97db429e62bdcc3d
xTEddie <edwardtranv1@gmail.com>
2018-10-20 19:38:37 -0400

    Add recording admin model

--- server/apps/recordings/admin.py
+++ server/apps/recordings/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from apps.recordings.models.recording import Recording
 
 
-admin.site.register(Recording)
+class RecordingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text', 'image')
+
+
+admin.site.register(Recording, RecordingAdmin)
