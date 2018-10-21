f8bf9ce4bbde7eb21b4629fb1b267e8706e31de4
Alexander <awstone@buffalo.edu>
2018-10-20 22:29:21 -0400

    new image assembler

--- server/utils/image_assembler.py
+++ server/utils/image_assembler.py
@@ -11,22 +11,49 @@ class image_assembler(object):
         img_dic = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'image_dictionary')
 
         image_dictionary = pickle.load(open(img_dic, 'rb'))
-        print(image_dictionary)
-        frame = image_dictionary[frame_info[0]['type']]
-        flip_img = cv2.flip(image_dictionary[frame_info[1]['type']], 1)
-        space = np.empty((256, 60))
+        foreground = image_dictionary[frame_info[0]['type']]
+        flip_foreground = cv2.flip(image_dictionary[frame_info[1]['type']], 1)
+        space = np.empty((384, 60))
         space.fill(255)
-        frame = np.hstack((frame, space))
-        frame = np.hstack((frame, flip_img))
-        background = cv2.imread(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'binarized_scene.png'), 0)
-        right_splice_bound = int (background.shape[1]/2)+286
-        left_splice_bound = int (background.shape[1]/2)-286
-        top_splice_bound = int (background.shape[0]/2)-78
-        bottom_splice_bound = int (background.shape[0]/2)+178
-        background[top_splice_bound:bottom_splice_bound, left_splice_bound:right_splice_bound] = frame
+        foreground = np.hstack((foreground, space))
+        foreground = np.hstack((foreground, flip_foreground))
+        background = cv2.imread("scene.png", 0)
+        alpha = np.zeros((384,384))
+        flip_alpha = np.zeros((384, 384))
+        alpha = np.hstack((alpha, space - 255))
+        alpha = np.hstack((alpha, flip_alpha))
+        print (foreground.shape)
+        print (alpha.shape)
+        for i in range (0, foreground.shape[0]):
+            for j in range (0, foreground.shape[1]):
+                if foreground[i, j] != 255:
+                    alpha[i, j] = 255
+
+        for i in range (0, flip_foreground.shape[0]):
+            for j in range (0, flip_foreground.shape[1]):
+                if flip_foreground[i, j] != 255:
+                    flip_alpha[i, j] = 255
+
+        right_splice_bound = int (background.shape[1]/2)+414
+        left_splice_bound = int (background.shape[1]/2)-414
+        top_splice_bound = int (background.shape[0]/2)+0
+        bottom_splice_bound = int (background.shape[0]/2)+384
+
+        print (foreground.shape)
+        print (alpha.shape)
+        slice = background[top_splice_bound:bottom_splice_bound, left_splice_bound:right_splice_bound]
+        print(slice.shape)
+        slice = slice - alpha
+
+        for i in range (0, slice.shape[0]):
+            for j in range (0, slice.shape[1]):
+                if slice[i, j] < 0:
+                    slice[i,j] = foreground[i, j]
+
+        background[top_splice_bound:bottom_splice_bound, left_splice_bound:right_splice_bound] = slice
         background = cv2.resize(background, (720, 509), interpolation=cv2.INTER_AREA)
-        output_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'output.png')
-        if save_path:
-            output_file_path = os.path.join(save_path, 'output.png')
-        cv2.imwrite(output_file_path, background)
-        return 'output.png'
+        cv2.imwrite('final_frame.png', background)
+        return 'final_frame.png'
+        # cv2.imshow('fig', background)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
