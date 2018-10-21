738ecf8381848acfe64c7d4b03859dc2443dcdaf
xTEddie <edwardtranv1@gmail.com>
2018-10-20 23:59:28 -0400

    Add placeholder to record for text

--- server/apps/recordings/views/recording.py
+++ server/apps/recordings/views/recording.py
@@ -13,6 +13,15 @@ from django.core.files.base import ContentFile
 class RecordView(APIView):
 
     def get(self, request):
+
+        record_type = self.request.GET.get('type')
+
+        # GET /record?type=text
+        if record_type == 'text':
+            return Response(status=status.HTTP_200_OK)
+
+        # Default
+
         data = get_speech_data()
         print(data)
 
@@ -34,6 +43,5 @@ class RecordView(APIView):
 
 
         serializer = RecordingSerializer(recording)
-
         return Response(serializer.data, status=status.HTTP_200_OK)
 
